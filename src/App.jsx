d12baccc075201fcbc9fd28b2855216bfa1c1ed6
mohamedaymaan <mohamd229955@gmail.com>
2025-09-22 20:30:00 +0300

    profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,7 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Layout from "./Layout/Layout";
-import Home from "./Pages/Home/Home";
-import Notification from "./Component/Notification";
-// Page Profile
-import ProfilePage from "./Profile/ProfilePage";
-import EditProfile from "./Profile/EditProfile";
-import Settings from "./Profile/Settings";
-import PaymentMethod from "./Profile/PaymentMethod";
-import EmptyCards from "./Profile/EmptyCards";
-import PasswordManagement from "./Profile/PasswordManagement";
-import VisaVersion from "./Profile/VisaVersion";
-import AddNewCard from "./Profile/AddNewCard";
-import FAQs from "./Profile/FAQs";
-import PrivacyPolicy from "./Profile/PrivacyPolicy";
-import Favorite from "./Component/Favorite";
-import Done from "./Profile/Done";
-// import CardsWrapper from "./Profile/CardsWrapper";
-
 import { createHashRouter, RouterProvider } from "react-router";
 import "./App.css";
 import Layout from "./Layout/Layout";
 import Home from "./Pages/Home/Home";
-
 import Appointment from "./Pages/Appointment/Appointment";
 import Doctors from "./Pages/Doctors/Doctors";
 import Favourite from "./Pages/Favourite/Favourite";
@@ -43,31 +23,260 @@ import { ToastContainer } from "react-toastify";
 import VerifyOTP from "./Pages/auth/VerifyOTP";
 import ForgetPassword from "./Pages/auth/ForgetPassword";
 import ResetPassword from "./Pages/auth/ResetPassword";
-import Test from "./Pages/Test";
 import { ProtectUser } from "./protectedRouting/ProtectedRouting";
 import LayoutWithoutNav from "./Layout/LayoutwithoutNav";
+import ProfilePage from "./Profile/ProfilePage";
+import EditProfile from "./Profile/EditProfile";
+import Settings from "./Profile/Settings";
+import PaymentMethod from "./Profile/PaymentMethod";
+import EmptyCards from "./Profile/EmptyCards";
+import PasswordManagement from "./Profile/PasswordManagement";
+import VisaVersion from "./Profile/VisaVersion";
+import AddNewCard from "./Profile/AddNewCard";
+import FAQs from "./Profile/FAQs";
+import PrivacyPolicy from "./Profile/PrivacyPolicy";
+import Favorite from "./Component/Favorite";
+import Done from "./Profile/Done";
 function App() {
+  useEffect(() => {
+    initFlowbite();
+  }, []);
 
-const router = createHashRouter([
-  {
-    path:'/',
-    element:<Layout /> ,
-    children:[
-  {
-    index:true, 
-    element:<Home/>
-  }, 
+  const router = createHashRouter([
     {
-      path:'home', 
-      element:<Home/>
-    }
-  ]},
-])
-   return (
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          index: true,
+          element: (
+            <ProtectUser>
+              <Home />
+            </ProtectUser>
+          ),
+        },
+        {
+          path: "home",
+          element: (
+            <ProtectUser>
+              <Home />
+            </ProtectUser>
+          ),
+        },
+        {
+          path: "doctors",
+          element: (
+            <ProtectUser>
+              <Doctors />
+            </ProtectUser>
+          ),
+        },
+        {
+          path: "favourite",
+          element: (
+            <ProtectUser>
+              <Favourite />
+            </ProtectUser>
+          ),
+        },
+        {
+          path: "specialist",
+          element: (
+            <ProtectUser>
+              <Specialist />
+            </ProtectUser>
+          ),
+        },
+        {
+          path: "search",
+          element: (
+            <ProtectUser>
+              <Search />
+            </ProtectUser>
+          ),
+        },
+        {
+          path: "map",
+          element: (
+            <ProtectUser>
+              <Mappage />
+            </ProtectUser>
+          ),
+        },
+        {
+          path: "searchlocation",
+          element: (
+            <ProtectUser>
+              <SearchLocation />
+            </ProtectUser>
+          ),
+        },
+
+        {
+          path: "loadingmap",
+          element: (
+            <ProtectUser>
+              <LoadingMap />
+            </ProtectUser>
+          ),
+        },
+        {
+          path: "searchresult",
+          element: (
+            <ProtectUser>
+              <SearchResult />
+            </ProtectUser>
+          ),
+        },
+        {
+          path: "the map",
+          element: (
+            <ProtectUser>
+              <PhoneMap />
+            </ProtectUser>
+          ),
+        },
+        {
+          path: "appointment",
+          element: (
+            <ProtectUser>
+              <Appointment />
+            </ProtectUser>
+          ),
+        },
+        {
+          path: "booking",
+          element: (
+            <ProtectUser>
+              <Booking />
+            </ProtectUser>
+          ),
+        },
+        {
+          path: "ProfilePage",
+          element: (
+            <ProtectUser>
+              <ProfilePage />
+            </ProtectUser>
+          ),
+        },
+        {
+          path: "EditProfile",
+          element: (
+            <ProtectUser>
+              <EditProfile />
+            </ProtectUser>
+          ),
+        },
+        {
+          path: "Settings",
+          element: (
+            <ProtectUser>
+              <Settings />
+            </ProtectUser>
+          ),
+        },
+        {
+          path: "PaymentMethod",
+          element: (
+            <ProtectUser>
+              <PaymentMethod />
+            </ProtectUser>
+          ),
+        },
+        {
+          path: "EmptyCards",
+          element: (
+            <ProtectUser>
+              <EmptyCards />
+            </ProtectUser>
+          ),
+        },
+        {
+          path: "PasswordManagement",
+          element: (
+            <ProtectUser>
+              <PasswordManagement />
+            </ProtectUser>
+          ),
+        },
+        {
+          path: "VisaVersion",
+          element: (
+            <ProtectUser>
+              <VisaVersion />
+            </ProtectUser>
+          ),
+        },
+        {
+          path: "FAQs",
+          element: (
+            <ProtectUser>
+              <FAQs />
+            </ProtectUser>
+          ),
+        },
+        {
+          path: "PrivacyPolicy",
+          element: (
+            <ProtectUser>
+              <PrivacyPolicy />
+            </ProtectUser>
+          ),
+        },
+        {
+          path: "Favorite",
+          element: (
+            <ProtectUser>
+              <Favorite />
+            </ProtectUser>
+          ),
+        },
+        {
+          path: "Done",
+          element: (
+            <ProtectUser>
+              <Done />
+            </ProtectUser>
+          ),
+        },
+      ],
+    },
+    {
+      path: "/",
+      element: <LayoutWithoutNav />,
+      children: [
+        {
+          path: "login",
+          element: <Login />,
+        },
+        {
+          path: "register",
+          element: <SignUp />,
+        },
+        {
+          path: "forgetpass",
+          element: <ForgetPassword />,
+        },
+        {
+          path: "verifyotp",
+          element: <VerifyOTP />,
+        },
+        {
+          path: "resetpassword",
+          element: <ResetPassword />,
+        },
+        { path: "*", element: "<Notfound />" },
+      ],
+    },
+  ]);
+  return (
     <>
-    <RouterProvider router={router} /> 
-      </>
-  )
+      <RouterProvider router={router} />
+      <Toaster />
+      <ToastContainer position="top-center" autoClose={3000} />
+    </>
+  );
 }
 
-export default App
+export default App;
